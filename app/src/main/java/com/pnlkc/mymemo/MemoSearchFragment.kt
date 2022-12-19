68de080a8a6ae457cc7902e61d73aabbf2c22afc
Jichan Choi <pnlkc@naver.com>
2022-12-19 15:34:11 +0900

    1.9.4

--- app/src/main/java/com/pnlkc/mymemo/MemoSearchFragment.kt
+++ app/src/main/java/com/pnlkc/mymemo/MemoSearchFragment.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
